6beabcefb8f51720477a4e662626064ec1a3ad13
Chris <csteel@gmail.com>
2024-10-25 21:19:25 +0100

    Add excerpt and code styling

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,28 @@
 import eleventySass from "@11tyrocks/eleventy-plugin-sass-lightningcss";
+import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
 
 export default (eleventyConfig) => {
   // Compile Sass in Eleventy (11ty) and process it with LightningCSS to minify, prefix, and add future CSS support.
   eleventyConfig.addPlugin(eleventySass);
+  // Add syntax highlighting at build time via prismJS
+  eleventyConfig.addPlugin(syntaxHighlight);
   // Add current year shortcode {% year %}
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+  // Add date formatter
+  eleventyConfig.addFilter("longDate", (dateString) => {
+    const dateObj = new Date(dateString);
+    return dateObj.toLocaleString(undefined, {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+  });
+
+  // eleventyConfig.setFrontMatterParsingOptions({
+  //   excerpt: true,
+  //   excerpt_alias: "excerpt",
+  //   excerpt_separator: "<!-- excerpt -->",
+  // });
 
   return {
     dir: {
---
+++ src/blog/adding-a-monthly-grouped-post-list-to-11ty.md
@@ -0,0 +1,13 @@
+---
+title: Adding a monthly grouped page list to 11ty
+excerpt: Grouping posts by their publish date is a popular way to improve readability and organization in blog archives, but achieving this programmatically can be a little tricky. In this post, we’ll walk through a simple solution that sorts posts by month and year for a 11ty site.
+---
+
+Adding some syntax highlighting in for good measure...
+
+```javascript
+const msg = "hello world";
+console.log(msg);
+```
+
+We should normally have another paragraph under this one.
